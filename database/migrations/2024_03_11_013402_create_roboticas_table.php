d1910386ab39d6d8ca056f18eaaaab8f21e4e430
SamuelHerreraa <157838017+SamuelHerreraa@users.noreply.github.com>
2024-03-10 20:59:14 -0500

    Update 2024_03_11_013402_create_roboticas_table.php

--- database/migrations/2024_03_11_013402_create_roboticas_table.php
+++ database/migrations/2024_03_11_013402_create_roboticas_table.php
@@ -12,7 +12,8 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('roboticas', function (Blueprint $table) {
-            $table->id();
+            $table->id('id_kit');
+            $table->string('nombre_kit');
             $table->timestamps();
         });
     }
